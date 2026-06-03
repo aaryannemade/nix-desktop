5a5d07bb9375e1d8d893960fe45b8d1eab6810d0
aaryan <me@aaryandesigns.games>
2026-06-03 21:48:59 +0530

    Added Basic subdirectories to home if missing.

--- home.nix
+++ home.nix
@@ -1,17 +1,26 @@
 { config, lib, pkgs, ... }:
 
 let 
-  # Import shared helper function
+  # Import shared helper functions
   importModules = import ./helpers/importModules.nix { inherit lib; };
+  createDirectories = import ./helpers/createDirectories.nix { inherit lib config; };
 in
 {
     home.username = "aaryan";
     home.homeDirectory = "/home/aaryan";
     home.stateVersion = "25.05";
 
-    home.activation.createPicturesDirectories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      mkdir -p "$HOME/Pictures/Screenshots"
-    '';
+    # Create standard home directories on activation
+    home.activation.createHomeDirectories = createDirectories [
+      "Desktop"
+      "Documents"
+      "Downloads"
+      "Pictures"
+      "Pictures/Screenshots"
+      "Music"
+      "Videos"
+      "Projects"
+    ];
 
     imports = 
       (importModules ./modules) ++
