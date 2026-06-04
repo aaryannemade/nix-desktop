7d08e6237b0c91a1812791cdc5884d9f3e5ac788
Aaryan <aaryan.nemade@pm.me>
2026-06-04 11:49:30 +0530

    Final implementation of xdg userdirs

--- home.nix
+++ home.nix
@@ -3,7 +3,6 @@
 let 
   # Import shared helper functions
   importModules = import ./helpers/importModules.nix { inherit lib; };
-  createDirectories = import ./helpers/createDirectories.nix { inherit lib config; };
 in
 {
     home.username = "aaryan";
@@ -15,17 +14,10 @@ in
       userDirs = {
         enable = true;
         createDirectories = true;
-        desktop = "${config.home.homeDirectory}/desktop";
-        documents = "${config.home.homeDirectory}/documents";
-        download = "${config.home.homeDirectory}/downloads";
-        pictures = "${config.home.homeDirectory}/pictures";
-        music = "${config.home.homeDirectory}/music";
-        videos = "${config.home.homeDirectory}/videos";
-        projects = "${config.home.homeDirectory}/projects";
         extraConfig = {
-          screenshots = "${config.home.homeDirectory}/pictures/screenshots";
-          screenrecordings = "${config.home.homeDirectory}/videos/screenrecordings";
-          trash = "${config.home.homeDirectory}/trash";
+          screenshots = "${config.home.homeDirectory}/Pictures/screenshots";
+          screenrecordings = "${config.home.homeDirectory}/Videos/screenrecordings";
+          trash = "${config.home.homeDirectory}/Trash";
         };
       };
     };
