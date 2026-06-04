3c77e5e6a453de0b488fed424fca310f4e7785cd
aaryan <me@aaryandesigns.games>
2026-06-04 13:07:10 +0530

    added nix formatter

--- flake.nix
+++ flake.nix
@@ -36,5 +36,7 @@
         nixosConfigurations = import ./hosts {
           inherit nixpkgs home-manager nur mangowm inputs;
         };
+
+        formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
     };
 }
