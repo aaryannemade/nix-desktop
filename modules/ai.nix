d4f17b6e27d859ed8b3aba92fb4f7c5652de49c5
Aaryan <aaryan.nemade@pm.me>
2026-06-10 15:40:35 +0530

    added deepseek api token in plain text to config

--- modules/ai.nix
+++ modules/ai.nix
@@ -8,6 +8,7 @@
   xdg.configFile."opencode/opencode.json".text = builtins.toJSON {
     "$schema" = "https://opencode.ai/config.json";
     plugin = [ "opencode-claude-auth@latest" ];
+    provider.deepseek.options.apiKey = "sk-b51b95ba7cc94627bc70e6b35457b8d1";
   };
 
   xdg.configFile."opencode/tui.json".text = builtins.toJSON {
