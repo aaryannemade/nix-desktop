1610f9c148d7cb7c64ffb5cd66f7db2a0bb016cf
Aaryan <aaryan.nemade@pm.me>
2026-06-13 10:05:57 +0530

    Removed Quickshell repo from flake input

--- flake.nix
+++ flake.nix
@@ -21,11 +21,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     noctalia.url = "github:noctalia-dev/noctalia";
 
     nvim-config = {
