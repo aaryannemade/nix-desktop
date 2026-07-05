c8a64077c634a9c3fb84486abc2f58003cf26688
Aaryan <aaryan.nemade@pm.me>
2026-07-05 22:27:04 +0530

    testing airplay client with pipewire and nixos

--- system/nixos/sound.nix
+++ system/nixos/sound.nix
@@ -5,8 +5,20 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    raopOpenFirewall = true;
     wireplumber = {
       enable = true;
+    };
+
+    extraConfig.pipewire."10-airplay" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-raop-discover";
+
+          # If AirPlay playback drops out, try setting "raop.latency.ms" here.
+          # args."raop.latency.ms" = 500;
+        }
+      ];
     };
   };
 }
